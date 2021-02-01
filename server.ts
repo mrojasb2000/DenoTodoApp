b625cfbb6ed691006cf13442353fd984e5bbc2eb
Mauricio Rojas <mrojasb2000@gmail.com>
2021-01-31 22:35:04 -0300

    Added format string color

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { Application, Response, Router } from "https://deno.land/x/oak/mod.ts";
+import { green, yellow } from "https://deno.land/std@0.85.0/fmt/colors.ts"
 
 const app = new Application();
 // deno-lint-ignore no-inferrable-types
@@ -17,6 +18,6 @@ app.use(router.allowedMethods());
 app.addEventListener("listen", ({ secure, hostname, port }) => {
   const protocol = secure ? "https://" : "http://";
   const url = `${protocol}${hostname ?? "localhost"}:${port}`;
-  console.log(`Listering on port ${port}`);
+  console.log(`${yellow("Listering on port")} ${green(url)}`);
 });
 await app.listen({ port });
