69b4ab70de1afe54e28ab41742f1c8756ed636dd
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-01 22:49:45 -0300

    Added get todo by id

--- services/todo.ts
+++ services/todo.ts
@@ -1,8 +1,8 @@
-import { Response  } from "https://deno.land/x/oak/mod.ts";
+import { Response } from "https://deno.land/x/oak/mod.ts";
 import todos from "../stubs/todos.ts";
 
 export default {
-  getAllTodos: ({response}:{response: Response}) => {
+  getAllTodos: ({ response }: { response: Response }) => {
     response.status = 200;
     response.body = {
       success: true,
@@ -10,7 +10,24 @@ export default {
     };
   },
   createTodo: () => {},
-  getTodoById: () => {},
+  getTodoById: (
+    { params, response }: { params: { id: string }; response: Response },
+  ) => {
+    const todoFound = todos.find(todo => todo.id === params.id)
+    if(!todoFound){
+      response.status = 404;
+      response.body = {
+        success: false,
+        message: "Data not found"
+      };
+      return;
+    }
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: todoFound,
+    };
+  },
   updateTodoById: () => {},
   deleteTodoById: () => {},
 };
