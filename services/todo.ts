720cd6896818e9c6f02be3414a6420946191461e
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-01 22:36:00 -0300

    Added get all todos

--- services/todo.ts
+++ services/todo.ts
@@ -1,5 +1,14 @@
+import { Response  } from "https://deno.land/x/oak/mod.ts";
+import todos from "../stubs/todos.ts";
+
 export default {
-  getAllTodos: () => {},
+  getAllTodos: ({response}:{response: Response}) => {
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: todos,
+    };
+  },
   createTodo: () => {},
   getTodoById: () => {},
   updateTodoById: () => {},
