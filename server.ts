652ca5843cdf06405b241b692d8e878157310095
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-01 21:59:42 -0300

    Added routes and refactor server

--- server.ts
+++ server.ts
@@ -1,19 +1,13 @@
 import { Application, Response, Router } from "https://deno.land/x/oak/mod.ts";
-import { green, yellow } from "https://deno.land/std@0.85.0/fmt/colors.ts"
+import { green, yellow } from "https://deno.land/std@0.85.0/fmt/colors.ts";
+import todoRouter from "./routes/todo.ts";
 
 const app = new Application();
 // deno-lint-ignore no-inferrable-types
 const port: number = 8080;
 
-const router = new Router();
-router.get("/", ({ response }: { response: Response }) => {
-  response.body = {
-    message: "hello world",
-  };
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(todoRouter.routes());
+app.use(todoRouter.allowedMethods());
 
 app.addEventListener("listen", ({ secure, hostname, port }) => {
   const protocol = secure ? "https://" : "http://";
---
+++ services/todo.ts
@@ -0,0 +1,7 @@
+export default {
+  getAllTodos: () => {},
+  createTodo: () => {},
+  getTodoById: () => {},
+  updateTodoById: () => {},
+  deleteTodoById: () => {},
+};
