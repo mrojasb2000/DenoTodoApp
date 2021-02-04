ee08a97eb15ace11ba3400eba7c8553e1d940fb7
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-03 23:33:17 -0300

    Added create todo and refactor code base

--- services/todo.ts
+++ services/todo.ts
@@ -1,5 +1,7 @@
-import { Response } from "https://deno.land/x/oak/mod.ts";
+import { Request, Response } from "https://deno.land/x/oak/mod.ts";
+import { v4 } from "https://deno.land/std@0.85.0/uuid/mod.ts";
 import todos from "../stubs/todos.ts";
+import Todo from "../interfaces/Todo.ts";
 
 export default {
   getAllTodos: ({ response }: { response: Response }) => {
@@ -9,24 +11,46 @@ export default {
       data: todos,
     };
   },
-  createTodo: () => {},
+  createTodo: async (
+    { request, response }: { request: Request; response: Response },
+  ) => {
+    try {
+      if (!request.hasBody) throw { status: 401, message: "Invalid input data" }
+      const { todo } = await request.body().value
+      const newTodo: Todo = {
+        id: v4.generate(),
+        todo: todo,
+        isCompleted: false,
+      }
+      todos.push(newTodo)
+      response.status = 200;
+      response.body = {
+        success: true,
+        data: todos,
+      }
+    } catch (error) {
+      const { status, message } = error
+      response.status = status
+      response.body = { message }
+    } 
+  },
+
   getTodoById: (
     { params, response }: { params: { id: string }; response: Response },
   ) => {
-    const todoFound = todos.find(todo => todo.id === params.id)
-    if(!todoFound){
-      response.status = 404;
+    try {
+      const todoFound = todos.find((todo) => todo.id === params.id);
+      if (!todoFound) throw { status: 404, message: "User not found" }
+      response.status = 200;
       response.body = {
-        success: false,
-        message: "Data not found"
-      };
-      return;
+        success: true,
+        data: todoFound,
+      }
+    } catch (error) {
+      const { status, message } = error
+      response.status = status
+      response.body = { message }
     }
-    response.status = 200;
-    response.body = {
-      success: true,
-      data: todoFound,
-    };
   },
   updateTodoById: () => {},
   deleteTodoById: () => {},
