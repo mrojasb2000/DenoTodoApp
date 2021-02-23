d968ad18bacad75fbde75977a753ef685938fa08
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-23 20:44:32 -0300

    Added middleware page not found

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import { Application, Response, Router } from "https://deno.land/x/oak/mod.ts";
 import { green, yellow } from "https://deno.land/std@0.85.0/fmt/colors.ts";
 import todoRouter from "./routes/todo.ts";
+import notFound from "./middlewares/notFound.ts"
 
 const app = new Application();
 // deno-lint-ignore no-inferrable-types
@@ -8,6 +9,8 @@ const port: number = 8080;
 
 app.use(todoRouter.routes());
 app.use(todoRouter.allowedMethods());
+// 404 page not found
+app.use(notFound)
 
 app.addEventListener("listen", ({ secure, hostname, port }) => {
   const protocol = secure ? "https://" : "http://";
